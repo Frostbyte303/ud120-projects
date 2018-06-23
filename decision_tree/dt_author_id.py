d12964fd40cc7a6bddc0921c872a3686b01a80ec
Mike <nordic.savoie@gmail.com>
2018-06-23 14:31:08 -0600

    Update dt_author_id.py

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,19 +12,26 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.tree import DecisionTreeClassifier
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
+t0 = time()
+clf2 = DecisionTreeClassifier(min_samples_split=40,random_state=0)
+clf2.fit(features_train, labels_train)
+print "classify training time:", round(time()-t0,3), "s"
 
+#### your code goes here
+
+acc_min_samples_split_2 = clf2.score(features_test, labels_test)
+
+def submitAccuracies():
+  return {"acc_min_samples_split_2":round(acc_min_samples_split_2,3)}
 
 #########################################################
 
